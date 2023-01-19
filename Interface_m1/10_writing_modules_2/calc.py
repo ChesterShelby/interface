af25d167b17d9790781029a4293a5cd4f00aff03
Vadim <NazarovvorazaN@mail.ru>
2023-01-19 22:30:48 +0600

    19.01.2023

--- Interface_m1/10_writing_modules_2/calc.py
+++ Interface_m1/10_writing_modules_2/calc.py
@@ -16,19 +16,19 @@ def div(num1: float, num2: float) -> str | float:
     return num1 / num2
 
 
-# if __name__ == '__main__':
-#     print('Я запущен как самостоятельная программа')
-#     choice = int(input('Выберите необходимое действие 1: +, 2: -, 3: *, 4: /\n'))
-#     num1 = int(input('Введите первое число: '))
-#     num2 = int(input('Введите второе число: '))
-#     match choice:
-#         case 1:
-#             print(add(num1, num2))
-#         case 2:
-#             print(sub(num1, num2))
-#         case 3:
-#             print(mul(num1, num2))
-#         case 4:
-#             print(div(num1, num2))
-#         case _:
-#             print('Неверный выбор')
+if __name__ == '__main__':
+    print('Я запущен как самостоятельная программа')
+    choice = int(input('Выберите необходимое действие 1: +, 2: -, 3: *, 4: /\n'))
+    num1 = int(input('Введите первое число: '))
+    num2 = int(input('Введите второе число: '))
+    match choice:
+        case 1:
+            print(add(num1, num2))
+        case 2:
+            print(sub(num1, num2))
+        case 3:
+            print(mul(num1, num2))
+        case 4:
+            print(div(num1, num2))
+        case _:
+            print('Неверный выбор')
